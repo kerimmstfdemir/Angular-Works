89413d34113d16c6dc7e1fd2ccaae7062f4f3cb5
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-09-07 16:27:37 +0300

    using *ngIf

--- angular-work-file/src/app/servers/servers.component.ts
+++ angular-work-file/src/app/servers/servers.component.ts
@@ -16,6 +16,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +28,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
