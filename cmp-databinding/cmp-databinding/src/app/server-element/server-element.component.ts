75aa8b53a291e9098fa9ce20231fcc0b89607e8a
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-09-19 17:10:32 +0300

    view encapsulation

--- cmp-databinding/cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
+import { ViewEncapsulation } from '@angular/compiler';
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: { type:string, name:string, content:string }
